1e941448c4dede32473d4c974248a9b09dff0402
Lucas Soares <soaresl472@gmail.com>
2025-01-29 18:03:16 -0300

    Added apoiame modal

--- src/components/Product/stories.tsx
+++
@@ -1,7 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react'
-import Product from '.'
-export default {
-  title: 'Product',
-  component: Product
-} as Meta
-export const Default: StoryObj = {}
